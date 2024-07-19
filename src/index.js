6d0db96434f9a0507601884d595f8c98eb8464e1
Mahaning <hmaning45@gmail.com>
2024-07-19 13:30:21 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AuthProvider } from './contxt/auth';
+import { SearchProvider } from './contxt/search';
+import { CartProvider } from './contxt/cart';
+
+// index.js or App.js
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthProvider>
+    <SearchProvider>
+      <CartProvider>
+    <BrowserRouter>
+      {/* <React.StrictMode> */}
+      <App />
+      {/* </React.StrictMode> */}
+    </BrowserRouter>
+    </CartProvider>
+    </SearchProvider>
+  </AuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
